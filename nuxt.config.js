3d3fee5be76469dbfc325c1af083d26055f25ed9
Kiyohiko Heima <piyo326@gmail.com>
2020-08-20 13:19:11 +0900

    Use signin API

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['~/plugins/geolocation', '~/plugins/dayjs'],
+  plugins: ['~/plugins/geolocation', '~/plugins/dayjs', '~/plugins/axios'],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
