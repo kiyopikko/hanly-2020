2f7e316b8b02b1f9a9d5dffee9cce038057c298d
Kiyohiko Heima <piyo326@gmail.com>
2020-08-20 14:49:23 +0900

    Use real data to my page

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -14,18 +14,24 @@
 
 <script>
 export default {
-  data() {
-    return {
-      nickname: 'my name',
-      latitude: 0,
-      longitude: 0,
-      datetime: new Date(),
-      face_image_url: '',
-    }
-  },
   computed: {
     getDateTime() {
       return this.$dayjs(this.datetime).format('YYYY/MM/DD HH:mm')
+    },
+    nickname() {
+      return this.$store.getters['me/nickname']
+    },
+    latitude() {
+      return this.$store.getters['me/latitude']
+    },
+    longitude() {
+      return this.$store.getters['me/longitude']
+    },
+    datetime() {
+      return this.$store.getters['me/datetime']
+    },
+    face_image_url() {
+      return this.$store.getters['me/face_image_url']
     },
   },
   methods: {
